c1099f66707cebe1bd69633488dce528ad98bac2
Pomid0rkaaa <artem.minecratt@gmail.com>
2026-02-03 15:29:38 +0200

    added args reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
+double eval(const string& expr);
+
 int main(int argc, char* args[]) {
-  cout << "Hello world!";
-  return 0;
+  string expr = "";
+  for (int i = 1; i < argc; i++) {
+    expr += args[i];
+  }
+  if (expr.empty()) return EXIT_FAILURE;
+  cout << eval(expr) << endl;
+  return EXIT_SUCCESS;
+}
+
+double eval(const string& expr) {
+  return expr.size();
 }
 
