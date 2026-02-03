49ad027178b485a7b5fd65d9bd21ea54cb099999
Pomid0rkaaa <artem.minecratt@gmail.com>
2026-02-03 15:43:47 +0200

    parser class and number parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cctype>
+#include <stdexcept>
 
 using namespace std;
+
+class Parser {
+  const string& s;
+  size_t pos = 0;
+
+public:
+  Parser(const string& str) : s(str) {}
+  double parse() {
+    size_t start = pos;
+    while (pos < s.size() && (isdigit(s[pos]) || s[pos] == '.')) pos++;
+    if (start == pos) throw runtime_error("Expected number");
+    return stod(s.substr(start, pos - start));
+  }
+};
 
 double eval(const string& expr);
 
@@ -17,6 +33,7 @@ int main(int argc, char* args[]) {
 }
 
 double eval(const string& expr) {
-  return expr.size();
+  Parser p(expr);
+  return p.parse();
 }
 
